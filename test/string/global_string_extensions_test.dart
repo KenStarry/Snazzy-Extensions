57d9dfe8859fdb2dd6168925e6c88d66a98f3f2f
KenStarry <starrycodes@gmail.com>
2024-04-06 19:39:22 +0300

    Global String extensions test

---
+++ test/string/global_string_extensions_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:snazzy_extensions/snazzy_extensions.dart';
+
+void main() {
+
+  group('Truncate Name', () {
+    test('Test one-worded names', () {
+      const name = 'John';
+
+      final truncatedName = name.truncateName;
+
+      expect(truncatedName, 'John');
+    });
+
+    test('Test two-worded names', () {
+      const name = 'John Doe';
+
+      final truncatedName = name.truncateName;
+
+      expect(truncatedName, 'John D.');
+    });
+
+    test('Test three-or-more-worded names', () {
+      const name = 'John Doe Splintzer K';
+
+      final truncatedName = name.truncateName;
+
+      expect(truncatedName, 'John D.');
+    });
+  });
+}
