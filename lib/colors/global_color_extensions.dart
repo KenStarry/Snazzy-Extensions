3cee14d6732ec1388cba65d4740e0951eff2c59b
KenStarry <starrycodes@gmail.com>
2024-04-09 14:31:42 +0300

    Added String extensions

--- lib/colors/global_color_extensions.dart
+++ lib/colors/global_color_extensions.dart
@@ -1,5 +1,30 @@
 part of snazzy_extensions;
 
 extension GlobalColorExtensions on Color {
+  /// Darken color by percent (100 -> black)
+  Color darkenColor([int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+
+    var f = 1 - percent / 100;
+    return Color.fromARGB(
+        alpha, (red * f).round(), (green * f).round(), (blue * f).round());
+  }
+
+  /// Lighten Color by percent (100 -> white)
+  Color lightenColor([int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+
+    var p = percent / 100;
+
+    return Color.fromARGB(alpha, red + ((255 - red) * p).round(),
+        green + ((255 - green) * p).round(), blue + ((255 - blue) * p).round());
+  }
+
+  /// Color to Hex
+  String get toHexString => '#${value.toRadixString(16)}';
+
+  /// Color to int value
+  int get toIntValue => value;
+
   /// Generate Material Color Palette from color
 }
