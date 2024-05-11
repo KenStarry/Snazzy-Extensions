2b94ed15ed2ece941a827bd3e6f8418c7912475d
KenStarry <starrycodes@gmail.com>
2024-05-11 15:18:53 +0300

    Added Intl package for date formatting

--- lib/snazzy_extensions.dart
+++ lib/snazzy_extensions.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:easy_localization/easy_localization.dart' as easy_locale;
 
 /// String Extensions
 
--- lib/string/date_string_extensions.dart
+++ lib/string/date_string_extensions.dart
@@ -1,30 +1,5 @@
 part of snazzy_extensions;
 
 extension DateTimeStringExtensions on String {
-  /// Parse date to day name
-  /// Parse date to day int
-  /// Parse date to month name
-  /// Parse date to month int
-  /// Parse date to year
-  /// Parse date to hours
-  /// Parse date to minutes
-  /// Parse date to seconds
-  /// Parse date to milliseconds
-  /// Parse date to DateTime obj
   /// Get number of days between dates
-  /// Get Current Hour in 24 hour format
-  String get szGetCurrentHourIn24 {
-    final date = DateTime.parse(this);
-
-    final formatter = easy_locale.DateFormat('HH');
-    return formatter.format(date);
-  }
-
-  /// Get Minutes From Time
-  String get szGetCurrentMinutes {
-    final date = DateTime.parse(this);
-
-    final formatter = easy_locale.DateFormat('mm');
-    return formatter.format(date);
-  }
 }
